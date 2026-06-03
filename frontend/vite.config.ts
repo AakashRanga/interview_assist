ef27f1ffbe0d2a5392b972c4bba0add0e65f14b4
clarencepraison <clarencepraison32303@gmail.com>
2026-06-03 12:20:10 +0530

    add git to live

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/interview_assist/',
   server: {
     port: 8189,
     host: '0.0.0.0',
